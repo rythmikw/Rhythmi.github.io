dbe70310f136643e97f895d366728ac0b6d710dd
rythmikw <rhythmi.info@gmail.com>
2023-12-26 18:59:06 +0300

    updates add expire date

--- credentials.js
+++ credentials.js
@@ -1,11 +1,11 @@
 // credentials.js
 var validCredentials = {
-    '51160': 'Aa96991995',
-    '49001': 'ni002',
-    'admin': '00000',
-    'StartUp': '2024',
-    'Ebrahim': '0002',
-    'wahab': '9699',
+    '51160': { password: 'Aa96991995', expires: '9999-12-27' },
+    '49001': { password: 'ni002', expires: '9999-12-31' },
+    'admin': { password: '00000', expires: '2025-12-25' },
+    'StartUp': { password: '2024', expires: '2024-04-10' },
+    'Ebrahim': { password: '0002', expires: '2023-12-31' },
+    'wahab': { password: '9699', expires: '2023-12-6' }
 };
 
 // Store in local storage
