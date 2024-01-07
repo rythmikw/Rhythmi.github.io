d874fd51a7a0921d801ed1681bc4f8c358a5a3a8
NiohFujimori <135440008+NiohFujimori@users.noreply.github.com>
2024-01-07 19:54:34 +0300

    Update app.py
    new edit

--- app/app.py
+++ app/app.py
@@ -20,7 +20,7 @@ images = "images"
 project_directory = os.path.dirname(os.path.abspath(__file__))
 model_path = os.path.join(project_directory, "raw.h5")
 output_file_path = os.path.join(project_directory, static_directory, "output.pdf")
-#logo = os.path.join(os.path.dirname(project_directory), images, "resulttemp.png")
+logo = os.path.join(os.path.dirname(project_directory), images, "resulttemp.png")
 
 
 app = Flask(__name__)
@@ -158,7 +158,7 @@ def process_ecg_file(file_path):
         plt.savefig(temp_file.name,format='png')
 
         # Use correct variable name here
-        #pdf.image(logo,x=0, y=0, w = 297.01 , h = 420.03)
+        pdf.image(logo,x=0, y=0, w = 297.01 , h = 420.03)
 
         kuwait_timezone = pytz.timezone("Asia/Kuwait")
 
