3469c3fb7f2e55474023c2c95d33e4762545ac60
rythmikw <rhythmi.info@gmail.com>
2024-01-13 03:22:30 +0300

    Update credentials.js

--- credentials.js
+++ credentials.js
@@ -6,10 +6,7 @@ var validCredentials = {
     'StartUp': { password: '2024', expires: '2024-04-10' },
     'Ebrahim': { password: '0002', expires: '2023-12-31' },
     'wahab': { password: '9699', expires: '2023-12-6' },
-
-
     'AUM': { password: '2024', expires: '2025-1-16' },
-
     'Alaa.Eleyan': { password: '2024', expires: '2025-1-16' },
     'Khaled.Chaine' : { password: '2024', expires: '2025-1-16' },
     'Bilal.Jabakhanji': { password: '2024', expires: '2025-1-16' },
@@ -19,7 +16,7 @@ var validCredentials = {
     'Fahmi.El-Sayed': { password: '2024', expires: '2025-1-16' },
     'Mouhammad.AlAkkoumi': { password: '2024', expires: '2025-1-16' },
     'Mehmet.Karaman': { password: '2024', expires: '2025-1-16' },
-    'Wael.Farag': { password: '2024', expires: '2025-1-16' }, 
+    'Wael.Farag': { password: '2024', expires: '2025-1-16' } 
 };
 
 // Store in local storage
